06e31d176efc5e0ab57df13fb26f4d35cb655eef
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2022-12-16 23:27:00 +0530

    Added images part

--- src/components/Team.js
+++ src/components/Team.js
@@ -3,6 +3,7 @@ import React from "react";
 const Team = () => {
 
     return (
+        <>
         <section class="py-4 text-center container-fluid bg-secondary">
             <div class="row py-lg-5">
                 <div class="col-lg-6 col-md-8 mx-auto">
@@ -11,6 +12,51 @@ const Team = () => {
                 </div>
             </div>
         </section>
+
+        <div className="album py-5">
+        <div className="container">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        
+        <div class="col border-0 my-4">
+          <div class="card border-0">
+          <img src="https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=" class="card-img-top" alt="..."/>
+            <div class="card-body">
+              <h3 class="card-text text-center">Insert Name</h3>
+            </div>
+          </div>
+        </div>
+
+        <div class="col border-0 my-4">
+          <div class="card border-0">
+          <img src="https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=" class="card-img-top" alt="..."/>
+            <div class="card-body">
+              <h3 class="card-text text-center">Insert Name</h3>
+            </div>
+          </div>
+        </div>
+
+        <div class="col border-0 my-4">
+          <div class="card border-0">
+          <img src="https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=" class="card-img-top" alt="..."/>
+            <div class="card-body">
+              <h3 class="card-text text-center">Insert Name</h3>
+            </div>
+          </div>
+        </div>
+
+        <div class="col border-0 my-4">
+          <div class="card border-0">
+          <img src="https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=" class="card-img-top" alt="..."/>
+            <div class="card-body">
+              <h3 class="card-text text-center">Insert Name</h3>
+            </div>
+          </div>
+        </div>
+
+        </div>
+        </div>
+        </div>
+        </>
     )
 }
 
