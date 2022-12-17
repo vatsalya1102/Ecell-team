ca3f53262c809350dc1e1bd294c3308519f56da2
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2022-12-17 22:59:03 +0530

    Added first container containing the faculty incharge

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+    return (
+
+        <section class="py-4 text-center container-fluid bg-secondary">
+            <div class="row py-lg-5">
+                <div class="col-lg-6 col-md-8 mx-auto">
+                    <h1 class="fw-semi-bold">Our Team</h1>
+                    <p class="lead">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default Header
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,63 +1,23 @@
 import React from "react";
+import Header from "./Header";
+import '../App.css';
 
 const Team = () => {
 
-    return (
-        <>
-        <section class="py-4 text-center container-fluid bg-secondary">
-            <div class="row py-lg-5">
-                <div class="col-lg-6 col-md-8 mx-auto">
-                    <h1 class="fw-semi-bold">Our Team</h1>
-                    <p class="lead">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-                </div>
-            </div>
-        </section>
-
-        <div className="album py-5">
-        <div className="container">
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        
-        <div class="col border-0 my-4">
-          <div class="card border-0">
-          <img src="https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=" class="card-img-top" alt="..."/>
-            <div class="card-body">
-              <h3 class="card-text text-center">Insert Name</h3>
-            </div>
+  return (
+    <>
+      <Header />
+      <div className="container py-5 d-flex justify-content-center">
+        <div class="card border-0"  style={{width:"300px"}}>
+          <img src="https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h3 class="card-text text-center">Dr.Tapas Pilai</h3>
           </div>
         </div>
+      </div>
 
-        <div class="col border-0 my-4">
-          <div class="card border-0">
-          <img src="https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=" class="card-img-top" alt="..."/>
-            <div class="card-body">
-              <h3 class="card-text text-center">Insert Name</h3>
-            </div>
-          </div>
-        </div>
-
-        <div class="col border-0 my-4">
-          <div class="card border-0">
-          <img src="https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=" class="card-img-top" alt="..."/>
-            <div class="card-body">
-              <h3 class="card-text text-center">Insert Name</h3>
-            </div>
-          </div>
-        </div>
-
-        <div class="col border-0 my-4">
-          <div class="card border-0">
-          <img src="https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=" class="card-img-top" alt="..."/>
-            <div class="card-body">
-              <h3 class="card-text text-center">Insert Name</h3>
-            </div>
-          </div>
-        </div>
-
-        </div>
-        </div>
-        </div>
-        </>
-    )
+    </>
+  )
 }
 
 export default Team
