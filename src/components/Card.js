6b1beb35db2ed14fde95688100d484c3e972c901
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2022-12-18 12:39:46 +0530

    Added hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import '../App.css';
 
 function Card(props) {
   return (
-    <div className="card-comp col-md-6 d-flex justify-content-center my-3">
+    <div className="col-md-6 d-flex justify-content-center my-3">
       <div class="card border-0" style={{ width: "300px" }}>
+      <div className="img-wrapper">
         <img src="https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=" class="card-img-top" alt="..." />
+        <h3 className='fade'>Executive</h3>
+        </div>
         <div class="card-body">
-          <h3 class="card-text text-center">{props.name}</h3>
+          <h4 class="card-text text-center fw-normal">{props.name}</h4>
         </div>
       </div>
     </div>
