95ee2eb62448a9bc27cc9e4cf03a0f65fb37fa7b
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2022-12-18 10:59:25 +0530

    Updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../App.css';
 
 function Card(props) {
   return (
-    <div className="col-md-6 d-flex justify-content-center my-3">
+    <div className="card-comp col-md-6 d-flex justify-content-center my-3">
       <div class="card border-0" style={{ width: "300px" }}>
         <img src="https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=" class="card-img-top" alt="..." />
         <div class="card-body">
